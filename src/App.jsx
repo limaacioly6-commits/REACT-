6231c5d241a45530aa7da87007c098eeb8273e38
AciolyLima.dev <limaacioly6@gmail.com>
2026-03-10 20:58:52 -0300

    REACT-1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './components/Button/Button.jsx';
+import Paragrafo from './components/Button/Paragrafo/Paragrafo.jsx';
+
+export default function App(){
+return (
+<div>
+  <h1>Estou Aprendendo React</h1>
+  <Button />
+  <Paragrafo />
+</div>
+);
+
+}
---
+++ src/components/Button/Paragrafo/Paragrafo.jsx
@@ -0,0 +1,5 @@
+export default function Paragrafo() {
+    return (
+        <p>Meu Parágrafo</p>
+    );
+}
