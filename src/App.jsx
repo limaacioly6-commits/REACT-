880173ca49ed6ba6a36254469792418bb0af5889
AciolyLima.dev <limaacioly6@gmail.com>
2026-03-10 22:01:11 -0300

    REACT-1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Button from './components/Button/Button.jsx';
 import Paragrafo from './components/Button/Paragrafo/Paragrafo.jsx';
 
 export default function App(){
+  const meuNome = "Acioly";
+  const idade = 41;
 return (
 <div>
+   <Button />
   <h1>Estou Aprendendo React</h1>
-  <Button />
-  <Paragrafo />
+   <Paragrafo meuNome={meuNome} idade={idade} />
 </div>
 );
 
--- src/components/Button/Paragrafo/Paragrafo.jsx
+++ src/components/Button/Paragrafo/Paragrafo.jsx
@@ -1,5 +1,11 @@
 export default function Paragrafo() {
+    const meuNome = "Acioly";
+    const idade = 41;
     return (
-        <p>Meu Parágrafo</p>
+        <>
+            <p>Olá meu nome é {meuNome}</p>
+            <p>Minha idade é {idade}</p>
+            <p>Meu Parágrafo</p>
+        </>
     );
 }
